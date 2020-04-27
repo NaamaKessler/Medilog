24f55aee895ce761d748e2c322e1f22fbe800876
SergeyHazanov <38078324+SergeyHazanov@users.noreply.github.com>
2020-04-27 18:54:25 +0300

    first button

--- main.py
+++ main.py
@@ -3,10 +3,21 @@ from tkinter import *
 root = Tk()
 
 # Creating label widget
-myLabel = Label(root, text="Hello World!")
+# myLabel1 = Label(root, text="Hello World!")
+# myLabel2 = Label(root, text="Goodbye World")
+
+def myClick():
+	myLabel = Label(root, text="Look! I clicked a buttion!!")
+	myLabel.pack()
+
+
+myButton = Button(root, text="Click Me!", command=myClick)
 
 # Shoving it onto the screen
-myLabel.pack()
+
+myButton.pack()
+# myLabel1.grid(row=0, column=0)
+# myLabel2.grid(row=1, column=1)
 
 root.mainloop()
 
