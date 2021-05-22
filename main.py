e3df8d85b66e0e75cf87388c4a9d7de8e2755ce5
SergeyHazanov <38078324+SergeyHazanov@users.noreply.github.com>
2021-05-22 18:13:18 +0300

    main classes added

--- main.py
+++ main.py
@@ -1,4 +1,59 @@
+import pandas as pd
+
+
+class Roster:
+
+    def __init__(self):
+        pass
+
+
+class AssignTable:
+
+    def __init__(self):
+        pass
+
+
+class RequestTable:
+
+    def __init__(self):
+        pass
+
+
+class QuotaTable:
+
+    def __init__(self):
+        pass
+
+
+class Physician:
+
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+
+    @property
+    def full_name(self):
+        return self.first + ' ' + self.last
+
+    @full_name.setter
+    def full_name(self, name):
+        first, last = name.split(' ')
+        self.first = first
+        self.last = last
+
+
+class Senior(Physician):
+    pass
+
+
+class Resident(Physician):
+    pass
 
 
 if __name__ == '__main__':
-    print('hey')
+    doc1 = Physician('Henry', 'Loyed')
+    print(doc1.full_name)
+    doc1.full_name = 'Josh Rosie'
+    print(doc1.full_name)
+
+
